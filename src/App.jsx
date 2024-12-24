bbaee0ddf80887dce3ce31405dc0b7b19d4023d9
Schnretzl <wrhendley@gmail.com>
2024-12-24 15:37:28 -0600

    Add comment for task 4

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ import CharacterDetail from "./CharacterDetail";
 const publicKey = 'd3ee3ded50e57e5d9cace17c30b9d77a';
 const hash = 'eb6d157b816ba0f4968b2350df82cc5e';
 
+// Task 4: Integrating Components and Updating the User Interface
+
+//     In your main App component, integrate CharacterList and CharacterDetail.
+
+//     Ensure that the character detail view updates dynamically when a character thumbnail is clicked.
+
 function App() {
 
   return (
